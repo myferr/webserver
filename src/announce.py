258fe2538d2cf244f8c187521900b60feb0641d2
myferonguilded <michaelthecrumbyt1@gmail.com>
2024-04-20 14:34:07 -0500

    Stylize errors and announcement.

--- src/announce.py
+++ src/announce.py
@@ -1,10 +1,13 @@
 import colorama
+import webbrowser
 
 print()
 print("┌──────────────────────────────────────────┐");
 print("│                                          │");
 print(f"│   {colorama.Fore.MAGENTA}{colorama.Style.BRIGHT} Served! {colorama.Style.RESET_ALL}                              │");
 print("│                                          │");
-print(f"│   {colorama.Style.BRIGHT}- Local: {colorama.Style.RESET_ALL}https://localhost:3249        │");
-print(f"│   {colorama.Style.BRIGHT}- Network: {colorama.Style.RESET_ALL}https://127.0.0.1:3249      │");
+print(f"│   {colorama.Style.DIM}{colorama.Fore.MAGENTA}➜{colorama.Style.RESET_ALL}  {colorama.Style.BRIGHT}Local: {colorama.Style.RESET_ALL}https://localhost:3249       │");
+print(f"│   {colorama.Style.DIM}{colorama.Fore.MAGENTA}➜{colorama.Style.RESET_ALL}  {colorama.Style.BRIGHT}Network: {colorama.Style.RESET_ALL}https://127.0.0.1:3249     │");
 print("│                                          │");
+
+webbrowser.open_new_tab('https://localhost:3249')
--- src/errors/error1.py
+++ src/errors/error1.py
@@ -1,9 +1,9 @@
 import colorama
 
 print("┌─────────────────────────────────────────────────────────┐")
-print(f"│{colorama.Style.BRIGHT}{colorama.Back.RED}{colorama.Fore.WHITE}ERROR                                                    {colorama.Style.RESET_ALL}│")
+print(f"│{colorama.Style.BRIGHT}{colorama.Back.RED}ERROR                                                    {colorama.Style.RESET_ALL}│")
 print("│                                                         │")
 print("│ To run your server please use the following command:    │")
-print(f"│      ➡️   {colorama.Fore.MAGENTA}server --run{colorama.Style.RESET_ALL}                                   │")
+print(f"│    {colorama.Style.DIM}{colorama.Fore.MAGENTA}➜{colorama.Style.RESET_ALL}    {colorama.Fore.MAGENTA}{colorama.Style.BRIGHT}server --run{colorama.Style.RESET_ALL}                                    │")
 print("│                                                         │")
 print("└─────────────────────────────────────────────────────────┘")
